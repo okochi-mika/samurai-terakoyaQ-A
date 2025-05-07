37ee92fa6cbf903b798137a25e7f15e2dafb4d54
okochi-mika <hiyoko_m@icloud.com>
2025-05-07 18:50:37 +0900

    コードの修正

--- laravel-basic/routes/web.php
+++ laravel-basic/routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 // ルーティングを設定するコントローラを宣言する
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\RequestController;
-
+use App\Http\Controllers\ResponseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,13 +23,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/hello", [HElloController::class, 'index']};
+Route::get('/hello', [HelloController::class, 'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/vendors/create', [VendorController::class, 'create']);
+
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
 
 Route::get('/requests/create', [RequestController::class, 'create']);
 
 Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
 
+Route::get('/responses', [ResponseController::class, 'index']);
