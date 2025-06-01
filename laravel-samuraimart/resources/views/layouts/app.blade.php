473c7b95cdec77eafdb3856a1446ad8824f47d4e
okochi-mika <hiyoko_m@icloud.com>
2025-06-01 17:39:49 +0900

    コードの編集

--- laravel-samuraimart/resources/views/layouts/app.blade.php
+++ laravel-samuraimart/resources/views/layouts/app.blade.php
@@ -20,9 +20,12 @@
             @component('components.header')
             @endcomponent
           
-            <main class="py-4">
+            <main class="py-4 mb-5">
                 @yield('content')
             </main>
+
+            @component('components.footer')
+            @endcomponent
         </div>
 
         <!-- Scripts -->
